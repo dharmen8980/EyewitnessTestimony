b3bede56e170e3090336b012556e280cfb94fb41
Dharmendra Sharma <dharmen8980@gmail.com>
2022-11-09 10:44:11 -0600

    changes for ui

--- client/pages/questions.js
+++ client/pages/questions.js
@@ -1,74 +1,70 @@
-import React, { useState, useEffect } from "react";
-import { Data } from "../components/data/survey";
+import React, { useState, useEffect, useMemo } from "react";
+import { SurveyQuestions } from "../components/data/survey";
+
+const createQuestions = (questions, onQuestionResponse) => {
+  console.log("here");
+  return questions.map((question) => (
+    <Question
+      key={question.id}
+      question={question}
+      onRadioClick={onQuestionResponse}
+    />
+  ));
+};
 
 const Questions = ({ onQuestionResponse, onComplete }) => {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(1);
 
   useEffect(() => {
     const timer = setTimeout(function () {
-      const maxQuestion = 4;
-      const isDone = currentQuestion === maxQuestion;
+      const isDone = currentQuestion === SurveyQuestions.length;
       if (isDone) {
         onComplete();
       } else {
         setCurrentQuestion(currentQuestion + 1);
       }
-    }, 2000);
+    }, 5000);
     return () => clearTimeout(timer);
   });
-  const onAnswer = onQuestionResponse;
-  const onRadioClick = (event) =>
-    onAnswer(Data[currentQuestion].name, event.target.value);
-  return (
-    <div>
-      {/* <button onClick={() => setCurrentQuestion(currentQuestion + 1)} className="text-white">NEXT</button> */}
-      <div id="question-1" className="mx-20">
-        <h3 className="text-white">Question {Data[currentQuestion].id}</h3>
-        <div className="py-16 grid grid-cols-2 gap-1 text-left">
-          <div className="w-fit">
-            <img
-              src={Data[currentQuestion].Subject1}
-              width={""}
-              height={""}
-              className="w-[450px] h-[500px]"
-            />
-          </div>
-          <div className="w-fit">
-            <img
-              src={Data[currentQuestion].Subject2}
-              width={""}
-              height={""}
-              className="w-[450px] h-[500px]"
-            />
-          </div>
-        </div>
-        <div>
-          <div className="mx-auto w-fit text-2xl text-yellow-400">
-            <p>Are these two people same?</p>
-            <button>
-            <input
-              type="radio"
-              value="yes"
-              name={Data[currentQuestion].type}
-              onClick={onRadioClick}
-            />
-            </button>
-            
-            <label className="mx-3">Yes</label>
-            <button>
-            <input
-              type="radio"
-              value="no"
-              name={Data[currentQuestion].type}
-              onClick={onRadioClick}
-            />
-            </button>
-            
-            <label className="mx-3">No</label>
-          </div>
-        </div>
+
+  const questions = useMemo(() => createQuestions(SurveyQuestions, onQuestionResponse), [SurveyQuestions, onQuestionResponse]);
+
+  return <div>{questions[currentQuestion - 1]}</div>;
+};
+
+const Question = ({ question, onRadioClick }) => (
+  <div id="question-1" className="mx-20">
+    <h3 className="text-white">Question {question.id}</h3>
+    <div className="py-16 grid grid-cols-2 gap-1 text-left">
+      <div className="w-fit">
+        <img src={question.subject1} className="w-[450px] h-[500px]" />
+      </div>
+      <div className="w-fit">
+        <img src={question.subject2} className="w-[450px] h-[500px]" />
       </div>
     </div>
-  );
-};
+    <div>
+      <div id={question.id} className="mx-auto w-fit text-2xl text-yellow-400">
+        <p>Are these two people same?</p>
+        <input
+          id={question.id}
+          type="radio"
+          value="yes"
+          name={question.name}
+          onClick={(event) => onRadioClick(question.name, event.target.value)}
+        />
+        <label className="mx-3">Yes</label>
+        <input
+          id={question.id}
+          type="radio"
+          value="no"
+          name={question.name}
+          onClick={(event) => onRadioClick(question.name, event.target.value)}
+        />
+        <label className="mx-3">No</label>
+      </div>
+    </div>
+  </div>
+);
+
 export default Questions;
